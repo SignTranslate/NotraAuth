eb3e1a6d88e7234d3148519368565085254e494d
karenxpn <karenmirakyan@gmail.com>
2024-03-28 16:09:43 +0400

    first commit

---
+++ Sources/NotraAuth/ViewModel/AuthViewRouter.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Karen Mirakyan on 28.03.24.
+//
+
+import Foundation
---
+++ Sources/NotraAuth/Views/Authentication.swift
@@ -0,0 +1,18 @@
+//
+//  Authenticate.swift
+//
+//
+//  Created by Karen Mirakyan on 28.03.24.
+//
+
+import SwiftUI
+
+struct Authenticate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Authenticate()
+}
